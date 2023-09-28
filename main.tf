dcefd6f9ba31b64efb4442ca66eb56f105c9e0de
Megh2210 <56001609+Megh2210@users.noreply.github.com>
2023-09-28 00:23:45 -0400

    provisioner 3

--- main.tf
+++ main.tf
@@ -114,12 +114,12 @@ resource "aws_instance" "server" {
     connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = "DevSecOps_1.pem"  # Replace with the path to your private key /workspaces/terraform-manifests/terraform-project/DevSecOps_1.pem
-    host        = self.public_ip
-    
+    private_key = "DevSecOps_1.pem"
+    host        = $(self.public_ip) 
   }
+
   provisioner "local-exec" {
-    command = "echo The server's IP address is ${self.public_ip}"
+    command = "echo The server's IP address is ${self.public_ip} "
   }
 
 
