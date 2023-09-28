8ac084ffeb031eb3187803a4a611126afc06c37b
Megh2210 <56001609+Megh2210@users.noreply.github.com>
2023-09-28 00:32:15 -0400

    connection update-01

--- main.tf
+++ main.tf
@@ -114,7 +114,7 @@ resource "aws_instance" "server" {
     connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = "DevSecOps_1.pem"
+    private_key = "DevSecOps_1"
     host        = self.public_ip 
   }
 
